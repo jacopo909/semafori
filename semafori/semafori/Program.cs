d2b83b509a5c81ccd9cedbb9526b8b3e30707501
jacopo909 <60032905+jacopo909@users.noreply.github.com>
2021-05-07 09:28:22 +0200

    Add files via upload

---
+++ semafori/semafori/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace semafori
+{
+    class Program
+    {
+        static int n;
+        static int incrementa;
+        static int decrementa;
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                
+                Thread t1 = new Thread(() => {
+                    incrementa = 1000000000;
+                    n += incrementa;
+                });
+                while (t1.IsAlive) ;
+                Thread t2 = new Thread(() => {
+                    decrementa = 1000000000;
+                    n += decrementa;
+                });
+                while (t2.IsAlive) ;
+                Console.WriteLine($"il numero n vale: {n}");
+            }
+            
+        }
+    }
+}
